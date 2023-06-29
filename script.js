cf028d01123284d2497552c1da1ecbd683e27d6b
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-29 16:47:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,10 +10,25 @@ const elements = document.querySelectorAll('.container');
 console.log(element);
 console.log(elements);
 
-const button = document.createElement('button');
-button.textContent = "newButton";
-button.style.width = "100px";
-button.style.height = "60px";
-button.style.fontSize = "18px";
+const button1 = document.createElement('button');
+button1.textContent = "newButton";
+button1.style.width = "100px";
+button1.style.height = "60px";
+button1.style.fontSize = "18px";
+button1.style.borderRadius = "6px";
 
-element.append(button);
+const button2 = document.createElement('button');
+button2.textContent = "newButton";
+button2.style.width = "100px";
+button2.style.height = "60px";
+button2.style.fontSize = "18px";
+button2.style.backgroundColor = "gray";
+button2.style.borderRadius = "6px";
+
+element.append(button1);
+element.prepend(button2);
+
+button1.addEventListener("click",() => {
+    button2.remove();
+})
+
