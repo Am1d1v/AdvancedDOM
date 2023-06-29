de270d140147e465f6ebd059078cd342c28b203e
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-29 16:29:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,16 @@
 //console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
+
+const element = document.querySelector('.container');
+const elements = document.querySelectorAll('.container');
+console.log(element);
+console.log(elements);
+
+const button = document.createElement('button');
+button.textContent = "newButton";
+button.style.width = "100px";
+button.style.height = "60px";
+button.style.fontSize = "18px";
+
+element.append(button);
