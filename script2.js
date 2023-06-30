d4d43c302640dd559f3961572d7558710c4de2ff
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-30 16:42:59 +0300

    Add files via upload

--- script2.js
+++ script2.js
@@ -8,6 +8,8 @@
 
 
 
+/*
+
 setInterval(() => {
     console.log(`Height: ${document.documentElement.clientHeight}`);
     console.log(`Width: ${document.documentElement.clientWidth}`);
@@ -15,8 +17,20 @@ setInterval(() => {
 
 },10000);
 
+*/
+
 const btn = document.querySelector('.container_Btn');
 
 btn.addEventListener("click",(e) =>{
     console.log(e.target.getBoundingClientRect());
+    window.scrollTo({
+        left: window.pageXOffset + rect.left,
+        top: window.pageYOffset + rect.top,
+        behavior: "smooth"
+    });
 });
+
+const element = document.querySelector('.down');
+const rect = element.getBoundingClientRect();
+console.log(element.getBoundingClientRect());
+
