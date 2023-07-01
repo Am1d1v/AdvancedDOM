64dbc318dd528ad24d0988482458d8cc0b3a5c35
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-01 17:08:33 +0300

    Add files via upload

--- eventsAppearance.js
+++ eventsAppearance.js
@@ -6,18 +6,24 @@ const innerWrapper = document.querySelector('.inner_wrapper');
 const inner = document.querySelector('.inner');
 const btn = document.querySelector('.inner_Btn');
 
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function(event) {
     this.style.backgroundColor = '#481752';
     this.style.opacity = '0.6';
-    console.log(btn);
+    //console.log(btn);
+    //console.log(event.target);
+    console.log(event.currentTarget);
 });
 
-inner.addEventListener('click', function() {
+inner.addEventListener('click', function(event) {
     this.style.backgroundColor = 'gray';
-    console.log(inner);
+    //console.log(inner);
+    //console.log(event.target);
+    console.log(event.currentTarget);
 });
 
-innerWrapper.addEventListener('click', function() {
+innerWrapper.addEventListener('click', function(event) {
     this.style.backgroundColor = 'purple';
-    console.log(innerWrapper);
+    //console.log(innerWrapper);
+    //console.log(event.target);
+    console.log(event.currentTarget);
 });
