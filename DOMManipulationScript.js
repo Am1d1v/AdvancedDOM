5eddeb85ae4a213857bb600feb12f31985a5bdfd
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-02 20:13:05 +0300

    Add files via upload

--- DOMManipulationScript.js
+++ DOMManipulationScript.js
@@ -1,5 +1,7 @@
 
 
+/*
+
 const wrapper = document.querySelector('.wrapper');
 console.log(wrapper);
 //console.log(wrapper.closest('body'));
@@ -11,6 +13,24 @@ const wrapperButton = document.querySelectorAll('.wrapper_button');
 console.log(wrapperButton);
 console.log(wrapperButton[0].closest('.wrapper'));
 
-console.log(wrapperButton[0].previousSibling)
-console.log(wrapperButton[0].nextElementSibling)
-console.log(wrapperButton[0].nextSibling)
+console.log(wrapperButton[0].previousSibling);
+console.log(wrapperButton[0].nextElementSibling);
+console.log(wrapperButton[0].nextSibling);
+
+*/
+
+document.addEventListener('DOMContentLoaded', function(e){
+    console.log('DOM Content Loaded');
+    console.log(e);
+});
+
+
+window.addEventListener('load', function(e){
+    console.log('All Content Loaded');
+    console.log(e);
+});
+
+window.addEventListener('beforeunload', function(e){
+   e.preventDefault();
+   e.returnValue = '';
+});
