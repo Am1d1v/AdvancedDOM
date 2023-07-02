b831f50af8b46d5c24426e054902b248a3fdd1ca
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-02 19:48:31 +0300

    Add files via upload

---
+++ DOMManipulationScript.js
@@ -0,0 +1,13 @@
+
+
+const wrapper = document.querySelector('.wrapper');
+console.log(wrapper);
+//console.log(wrapper.closest('body'));
+//console.log(wrapper.childNodes);
+//console.log([...wrapper.children][0]);
+console.log(wrapper.parentElement);
+
+const wrapperButton = document.querySelectorAll('.wrapper_button');
+console.log(wrapperButton);
+console.log(wrapperButton[0].closest('.wrapper'));
+
